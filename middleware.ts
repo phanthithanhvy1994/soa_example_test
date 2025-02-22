cc57bbb73a5177cca6667f511b43390c8e38440d
Vy Phan <phanthithanhvy1994@gmail.com>
2025-02-22 22:43:46 +0700

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,32 @@
 import { i18nRouter } from 'next-i18n-router'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
+import { Locale } from './types/language'
 
 export async function middleware(request) {
+  const res = await fetch(`${process.env.API_URL}/api/i18n/locales`, {
+    cache: 'no-cache',
+    headers: {
+      Authorization: `Bearer ${process.env.BEARER_TOKEN}`
+    }
+  })
 
+  const locales: Locale[] = await res.json()
   const availableLanguage = process.env.AVAILABLE_LANGUAGE?.split(',') || []
+  const availableLocales = locales.filter(l => availableLanguage.includes(l.code))
+  const defaultLocale = availableLocales.find(r => r.isDefault)
 
   const i18nResponse = i18nRouter(request, {
-    locales: availableLanguage.map(r => r),
-    defaultLocale: 'en'
+    locales: availableLocales.map(r => r.code),
+    defaultLocale: defaultLocale.code || 'en'
   })
 
   const response = i18nResponse || NextResponse.next()
   const cookiesStore = await cookies()
   const currentLocale = cookiesStore.get('NEXT_LOCALE')?.value
 
-  response.cookies.set('locales', JSON.stringify(availableLanguage), { path: '/' })
-  if (!currentLocale) response.cookies.set('NEXT_LOCALE',  'en')
+  response.cookies.set('locales', JSON.stringify(availableLocales), { path: '/' })
+  if (!currentLocale) response.cookies.set('NEXT_LOCALE', defaultLocale.code || 'en')
 
   return response
 }
