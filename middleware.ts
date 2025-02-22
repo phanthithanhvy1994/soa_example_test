67cf1886cde7f5654017e6b1553b13085b7b79fa
Vy Phan <phanthithanhvy1994@gmail.com>
2025-02-22 22:58:27 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,26 @@
-import { i18nRouter } from 'next-i18n-router'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
-import { Locale } from './types/language'
+import { i18nRouter } from 'next-i18n-router';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
-export async function middleware(request) {
-  const res = await fetch(`${process.env.API_URL}/api/i18n/locales`, {
-    cache: 'no-cache',
-    headers: {
-      Authorization: `Bearer ${process.env.BEARER_TOKEN}`
-    }
-  })
-
-  const locales: Locale[] = await res.json()
-  const availableLanguage = process.env.AVAILABLE_LANGUAGE?.split(',') || []
-  const availableLocales = locales.filter(l => availableLanguage.includes(l.code))
-  const defaultLocale = availableLocales.find(r => r.isDefault)
+export function middleware(request) {
+  const availableLanguage = process.env.AVAILABLE_LANGUAGE?.split(',') || [];
 
   const i18nResponse = i18nRouter(request, {
-    locales: availableLocales.map(r => r.code),
-    defaultLocale: defaultLocale.code || 'en'
-  })
+    locales: availableLanguage,
+    defaultLocale: 'en',
+  });
 
-  const response = i18nResponse || NextResponse.next()
-  const cookiesStore = await cookies()
-  const currentLocale = cookiesStore.get('NEXT_LOCALE')?.value
+  const response = i18nResponse || NextResponse.next();
 
-  response.cookies.set('locales', JSON.stringify(availableLocales), { path: '/' })
-  if (!currentLocale) response.cookies.set('NEXT_LOCALE', defaultLocale.code || 'en')
+  const cookiesStore = cookies();
+  const currentLocale = cookiesStore.get('NEXT_LOCALE')?.value;
 
-  return response
+  response.cookies.set('locales', JSON.stringify(availableLanguage), { path: '/' });
+  if (!currentLocale) response.cookies.set('NEXT_LOCALE', 'en');
+
+  return response;
 }
 
-// only applies this middleware to files in the app directory
 export const config = {
-  matcher: '/((?!api|static|.*\\..*|_next).*)'
+  matcher: '/((?!api|static|.*\\..*|_next).*)',
 }
